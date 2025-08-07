fba60f2e4ede265c341805fbb7b7a8eb0eec0158
Pallavi Bhagat <bhagatpallavi1@email.com>
2025-08-08 00:47:25 +0530

    Final changes

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+// Smooth scroll for navigation links
+document.querySelectorAll('.navlist a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    const href = this.getAttribute('href');
+    if (href.startsWith('#')) {
+      e.preventDefault();
+      document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
